e6d240aa22f75f269a590c42e69a419bf2b25768
Yequ Sun <yequsun@gmail.com>
2015-12-10 18:55:00 -0500

    Upload

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public float speed = 100.0f;
+
+	float hitFactor(Vector2 ballPos, Vector2 racketPos,float racketWidth) {
+		return (ballPos.x - racketPos.x) / racketWidth;
+	}
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+	
+	}
+	
+	// Update is called once per frame
+
+	void OnCollisionEnter2D(Collision2D col) {
+
+		if (col.gameObject.name == "racket") {
+
+			float x=hitFactor(transform.position,
+			                  col.transform.position,
+			                  col.collider.bounds.size.x);
+
+			Vector2 dir = new Vector2(x, 1).normalized;
+
+			GetComponent<Rigidbody2D>().velocity = dir * speed;
+		}
+
+	}
+}
---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+
+	public int health = 1;
+
+	void Start(){
+	}
+
+	void OnCollisionEnter2D(Collision2D collisionInfo) {
+		health -= 1;
+
+		if (health <= 0) {
+			Destroy(gameObject);
+		}
+	}
+}
